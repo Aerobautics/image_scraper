f8910a8a1bf253d01e619375a76fd917d1577be2
aerobautics <aerobautics@gmail.com>
2019-10-29 08:02:36 -0700

    Added prototype window

--- image_scraper.py
+++ image_scraper.py
@@ -1,2 +1,8 @@
-# This file is a placeholder.
-print("Hello, World!")
+#!/usr/bin/env python
+from Tkinter import *
+
+print("image_scraper")
+main_form = Tk()
+first_label = Label(main_form, text = "Image Scraper")
+first_label.pack()
+main_form.mainloop()
